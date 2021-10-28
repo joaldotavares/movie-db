55b3ece23155adba24544397f30536c1b15862ee
joaldotavares <joaldo.junior@ucsal.edu.br>
2021-10-28 11:38:17 -0300

    menu de opcoes na pagina inicial com navigation drawer

--- app/src/main/java/br/com/joaldo/moviesdbfilm/ui/MainActivity.kt
+++ app/src/main/java/br/com/joaldo/moviesdbfilm/ui/MainActivity.kt
@@ -2,37 +2,38 @@ package br.com.joaldo.moviesdbfilm.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import br.com.joaldo.moviesdbfilm.R
+import br.com.joaldo.moviesdbfilm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-
-    private val navHostFragment by lazy {
-        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-    }
-
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
     private val navController by lazy {
-        navHostFragment.navController
+        findNavController(R.id.nav_host_fragment)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.myToolBar)
-        setSupportActionBar(toolbar)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        drawerLayout = binding.drawer
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navigationView, navController)
+        navController.addOnDestinationChangedListener{_, destination, _->
+            title = destination.label
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp(appBarConfiguration) ||return super.onSupportNavigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout) || super.onSupportNavigateUp()
     }
 }
